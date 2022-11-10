cd5ea9782d02e4efd4170111994646c00611cf50
ajama99 <ajama@usc.edu>
2022-11-09 20:21:02 -0800

    add project 3

---
+++ project-3/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {  
+    $('.flower').click(function () {
+      $('.sentence').toggle();
+    });
+    
+    
+//    $('.computer').click(function () {
+//        $(this).toggleClass("bigger");
+//    });
+//     
+     $('.computer').click(function () {
+//        $('.infopanel').toggle(2000);     
+        $(this).toggleClass("bigger");
+    });
+    
+    });
